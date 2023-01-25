91c76a33fe23bc95a0d1a21d7b7ead96fc9bdabb
TomaszCitko <citkotomasz@gmail.com>
2023-01-25 13:20:41 +0100

    Change Board Title part 1

--- static/js/controller/boardsManager.js
+++ static/js/controller/boardsManager.js
@@ -1,93 +1,121 @@
-import {dataHandler} from "../data/dataHandler.js";
-import {htmlFactory, htmlTemplates} from "../view/htmlFactory.js";
-import {domManager} from "../view/domManager.js";
-import {cardsManager} from "./cardsManager.js";
-import {columnManager} from "./columnManager.js";
+import { dataHandler } from "../data/dataHandler.js";
+import { htmlFactory, htmlTemplates } from "../view/htmlFactory.js";
+import { domManager } from "../view/domManager.js";
+import { cardsManager } from "./cardsManager.js";
+import { columnManager } from "./columnManager.js";
 
 export let boardsManager = {
-    loadBoards: async function () {
-        const boards = await dataHandler.getBoards();
-        for (let board of boards) {
-            const boardBuilder = htmlFactory(htmlTemplates.board);
-            const content = boardBuilder(board);
-            domManager.addChild("#root", content);
-            domManager.addEventListener(
-                `.toggle-board-button[data-board-id="${board.id}"]`,
-                "click",
-                showHideButtonHandler
-            );
-            domManager.addEventListener(
-                `.board-add[data-board-id="${board.id}"]`,
-                'click',
-                newCardFormBuilder
-            )
-        }
-    },
-    initNewBoardButton: function () {
-      domManager.addEventListener(`#newBoard`, "click", addNewBoard);
-    },
+  loadBoards: async function () {
+    const boards = await dataHandler.getBoards();
+    for (let board of boards) {
+      const boardBuilder = htmlFactory(htmlTemplates.board);
+      const content = boardBuilder(board);
+      domManager.addChild("#root", content);
+      domManager.addEventListener(
+        `.toggle-board-button[data-board-id="${board.id}"]`,
+        "click",
+        showHideButtonHandler
+      );
+      domManager.addEventListener(
+        `.board-add[data-board-id="${board.id}"]`,
+        "click",
+        newCardFormBuilder
+      );
+      domManager.addEventListener(
+        `[board-title-id="${board.id}"]`,
+        "click",
+        changeBoardTitle
+      );
+    }
+  },
+  initNewBoardButton: function () {
+    domManager.addEventListener(`#newBoard`, "click", addNewBoard);
+  },
 };
 
 async function showHideButtonHandler(clickEvent) {
-    const boardId = clickEvent.target.dataset.boardId;
-    document.querySelector(`.board[data-board-id="${boardId}"] .board-header .board-toggle i`).classList.toggle('fa-chevron-down');
-    document.querySelector(`.board[data-board-id="${boardId}"] .board-header .board-toggle i`).classList.toggle('fa-chevron-up');
-    if (domManager.hasChildren(`.board[data-board-id="${boardId}"] .board-columns`)) {
-        domManager.deleteChildren(`.board[data-board-id="${boardId}"] .board-columns`);
-    } else {
-        await columnManager.loadColumns(boardId);
-        await cardsManager.loadCards(boardId);
-    }
+  const boardId = clickEvent.target.dataset.boardId;
+  document
+    .querySelector(
+      `.board[data-board-id="${boardId}"] .board-header .board-toggle i`
+    )
+    .classList.toggle("fa-chevron-down");
+  document
+    .querySelector(
+      `.board[data-board-id="${boardId}"] .board-header .board-toggle i`
+    )
+    .classList.toggle("fa-chevron-up");
+  if (
+    domManager.hasChildren(`.board[data-board-id="${boardId}"] .board-columns`)
+  ) {
+    domManager.deleteChildren(
+      `.board[data-board-id="${boardId}"] .board-columns`
+    );
+  } else {
+    await columnManager.loadColumns(boardId);
+    await cardsManager.loadCards(boardId);
+  }
 }
 
 async function addCardHandler(clickEvent) {
-    const boardId = clickEvent.target.dataset.boardId;
-    const title = clickEvent.target[0].value;
-    const status = clickEvent.target[1].value;
-    let newCard = await dataHandler.createNewCard(title, boardId, status, '1');
-    domManager.deleteChildren(`#newFormField`);
+  const boardId = clickEvent.target.dataset.boardId;
+  const title = clickEvent.target[0].value;
+  const status = clickEvent.target[1].value;
+  let newCard = await dataHandler.createNewCard(title, boardId, status, "1");
+  domManager.deleteChildren(`#newFormField`);
 }
 
 async function newCardFormBuilder(clickEvent) {
-    const boardId = clickEvent.target.dataset.boardId;
-    const content = `<br><form onsubmit="return false;" data-board-id="${boardId}">
+  const boardId = clickEvent.target.dataset.boardId;
+  const content = `<br><form onsubmit="return false;" data-board-id="${boardId}">
                         <input type="text" placeholder="Card Title" required>
                         <select id="statuses">
                             
                         </select>
                         <button type="submit">Save</button>
                     </form>`;
-    document.querySelector(`#newFormField`).innerHTML = content;
-    const statuses = await dataHandler.getStatuses(boardId)
-    const select = document.querySelector('#statuses');
-    for (let status of statuses) {
-        let option = document.createElement("option");
-        option.value = status.id;
-        option.text = status.title;
-        select.appendChild(option);
-    }
-    domManager.addEventListener(`#newFormField form`, 'submit', addCardHandler);
+  document.querySelector(`#newFormField`).innerHTML = content;
+  const statuses = await dataHandler.getStatuses(boardId);
+  const select = document.querySelector("#statuses");
+  for (let status of statuses) {
+    let option = document.createElement("option");
+    option.value = status.id;
+    option.text = status.title;
+    select.appendChild(option);
+  }
+  domManager.addEventListener(`#newFormField form`, "submit", addCardHandler);
 }
 
 function addNewBoard() {
-    const content = `<form onsubmit="return false;">
+  const content = `<form onsubmit="return false;">
                         <input type="text" placeholder="Board Title" required>
                         <button type="submit">Save</button>
                     </form>`;
-    document.querySelector(`#newFormField`).innerHTML = content;
-    domManager.addEventListener(`#newFormField form`, 'submit', submitNewBoard);
+  document.querySelector(`#newFormField`).innerHTML = content;
+  domManager.addEventListener(`#newFormField form`, "submit", submitNewBoard);
 }
 
 async function submitNewBoard(event) {
-    const title = event.currentTarget[0].value;
-    let board = (await dataHandler.createNewBoard(title))[0];
-    const boardBuilder = htmlFactory(htmlTemplates.board);
-    const content = boardBuilder(board);
-    domManager.addChild("#root", content);
-    domManager.addEventListener(
-        `.toggle-board-button[data-board-id="${board.id}"]`,
-        "click",
-        showHideButtonHandler
-    );
-    domManager.deleteChildren(`#newFormField`);
+  const title = event.currentTarget[0].value;
+  let board = (await dataHandler.createNewBoard(title))[0];
+  const boardBuilder = htmlFactory(htmlTemplates.board);
+  const content = boardBuilder(board);
+  domManager.addChild("#root", content);
+  domManager.addEventListener(
+    `.toggle-board-button[data-board-id="${board.id}"]`,
+    "click",
+    showHideButtonHandler
+  );
+  domManager.deleteChildren(`#newFormField`);
+}
+
+function changeBoardTitle(clickEvent) {
+  const boardTitleId = clickEvent.target.getAttribute("board-title-id");
+  const boardTitle = document.querySelector(
+    `[board-title-id="${boardTitleId}"]`
+  );
+  console.log(boardTitleId);
+  console.log(boardTitle);
+
+  domManager.changeDomBoardTitle(boardTitle);
 }
--- static/js/view/domManager.js
+++ static/js/view/domManager.js
@@ -1,42 +1,80 @@
 export let domManager = {
-    addChild(parentIdentifier, childContent) {
-        const parent = document.querySelector(parentIdentifier);
-        if (parent) {
-            parent.insertAdjacentHTML("beforeend", childContent);
-        } else {
-            console.error("could not find such html element: " + parentIdentifier);
-        }
-    },
-    addEventListener(parentIdentifier, eventType, eventHandler) {
-        const parent = document.querySelector(parentIdentifier);
-        if (parent) {
-            parent.addEventListener(eventType, eventHandler);
-        } else {
-            console.error("could not find such html element: " + parentIdentifier);
-        }
-    },
-    hasChildren(elementIdentifier) {
-        const parent = document.querySelector(elementIdentifier);
-        if (parent) {
-            return parent.hasChildNodes();
-        } else {
-            console.error("could not find such html element: " + elementIdentifier);
-        }
-    },
-    deleteChildren(elementIdentifier) {
-        const parent = document.querySelector(elementIdentifier);
-        if (parent) {
-            parent.innerHTML = '';
-        } else {
-            console.error("could not find such html element: " + elementIdentifier);
-        }
-    },
-    deleteElement(elementIdentifier) {
-        const parent = document.querySelector(elementIdentifier);
-        if (parent) {
-            parent.remove();
-        } else {
-            console.error("could not find such html element: " + elementIdentifier);
-        }
-    },
+  addChild(parentIdentifier, childContent) {
+    const parent = document.querySelector(parentIdentifier);
+    if (parent) {
+      parent.insertAdjacentHTML("beforeend", childContent);
+    } else {
+      console.error("could not find such html element: " + parentIdentifier);
+    }
+  },
+  addEventListener(parentIdentifier, eventType, eventHandler) {
+    const parent = document.querySelector(parentIdentifier);
+    if (parent) {
+      parent.addEventListener(eventType, eventHandler);
+    } else {
+      console.error("could not find such html element: " + parentIdentifier);
+    }
+  },
+  hasChildren(elementIdentifier) {
+    const parent = document.querySelector(elementIdentifier);
+    if (parent) {
+      return parent.hasChildNodes();
+    } else {
+      console.error("could not find such html element: " + elementIdentifier);
+    }
+  },
+  deleteChildren(elementIdentifier) {
+    const parent = document.querySelector(elementIdentifier);
+    if (parent) {
+      parent.innerHTML = "";
+    } else {
+      console.error("could not find such html element: " + elementIdentifier);
+    }
+  },
+  deleteElement(elementIdentifier) {
+    const parent = document.querySelector(elementIdentifier);
+    if (parent) {
+      parent.remove();
+    } else {
+      console.error("could not find such html element: " + elementIdentifier);
+    }
+  },
+
+  changeDomBoardTitle(boardTitle) {
+    boardTitle.onclick = function () {
+      console.log(
+        "kliknął Pan w " +
+          boardTitle.innerText +
+          ", następnym razem proszę bardziej uważać."
+      );
+      const inputText = document.createElement("input");
+      inputText.setAttribute("type", "text");
+      inputText.setAttribute("id", "tempTextBox");
+      inputText.setAttribute("value", boardTitle.innerText);
+      console.log(inputText);
+
+      const saveButton = document.createElement("button");
+      saveButton.setAttribute("type", "button");
+      saveButton.setAttribute("id", "saveButton");
+      saveButton.setAttribute("value", "Save");
+      saveButton.setAttribute("class", "Button");
+      saveButton.innerText = "Save";
+
+      const textSpace = boardTitle.nextSibling;
+      console.log("siotra" + textSpace);
+
+      boardTitle.replaceWith(inputText);
+      textSpace.replaceWith(saveButton);
+    };
+    return boardTitle;
+  },
+
+  saveDomBoardTitle(boardTitle) {
+    const inputText = document.getElementById("tempTextBox");
+    const saveButton = document.getElementById("saveButton");
+    const textSpace = " ";
+    boardTitle.innerText = inputText.value;
+    inputText.replaceWith(boardTitle);
+    saveButton.replaceWith(textSpace);
+  },
 };
