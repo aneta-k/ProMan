f957664299e556a5c8cb201aae5821eddbc69c29
TomaszCitko <citkotomasz@gmail.com>
2023-01-25 14:24:00 +0100

    error fix

--- static/js/data/dataHandler.js
+++ static/js/data/dataHandler.js
@@ -1,67 +1,65 @@
 export let dataHandler = {
-    getBoards: async function () {
-        return await apiGet("/api/boards");
-    },
-    getBoard: async function (boardId) {
-        // the board is retrieved and then the callback function is called with the board
-    },
-    getStatuses: async function () {
-        return await apiGet(`/api/statuses`);
-    },
-    getStatus: async function (statusId) {
-        // the status is retrieved and then the callback function is called with the status
-    },
-    getCardsByBoardId: async function (boardId) {
-        return await apiGet(`/api/boards/${boardId}/cards/`);
-    },
-    getCard: async function (cardId) {
-        // the card is retrieved and then the callback function is called with the card
-    },
-    createNewBoard: async function (boardTitle) {
-        return await apiPost(`/api/boards/create`, {'title':boardTitle});
-    },
-    createNewCard: async function (cardTitle, boardId, statusId, cardOrder) {
-        let card = {'status_id': statusId, 'title': cardTitle, 'card_order': cardOrder};
-        return await apiPost(`/api/boards/${boardId}/cards/add_new`, card);
-    },
-    deleteCard: async function (cardId) {
-        await apiDelete(`/api/cards/${cardId}/delete`);
-    },
-    changeBoardTitleApi: async function (boardID, title) {
-      let data =  {'boardId': boardID, 'title': title }
-      return await apiPut(`/api/board/${boardID}`, data)
-
+  getBoards: async function () {
+    return await apiGet("/api/boards");
+  },
+  getBoard: async function (boardId) {
+    // the board is retrieved and then the callback function is called with the board
+  },
+  getStatuses: async function () {
+    return await apiGet(`/api/statuses`);
+  },
+  getStatus: async function (statusId) {
+    // the status is retrieved and then the callback function is called with the status
+  },
+  getCardsByBoardId: async function (boardId) {
+    return await apiGet(`/api/boards/${boardId}/cards/`);
+  },
+  getCard: async function (cardId) {
+    // the card is retrieved and then the callback function is called with the card
+  },
+  createNewBoard: async function (boardTitle) {
+    return await apiPost(`/api/boards/create`, { title: boardTitle });
+  },
+  createNewCard: async function (cardTitle, boardId, statusId, cardOrder) {
+    let card = { status_id: statusId, title: cardTitle, card_order: cardOrder };
+    return await apiPost(`/api/boards/${boardId}/cards/add_new`, card);
+  },
+  deleteCard: async function (cardId) {
+    await apiDelete(`/api/cards/${cardId}/delete`);
+  },
+  changeBoardTitleApi: async function (boardID, title) {
+    let data = { boardId: boardID, title: title };
+    return await apiPut(`/api/board/${boardID}`, data);
+  },
 };
 
 async function apiGet(url) {
-    let response = await fetch(url, {
-        method: "GET",
-    });
-    if (response.ok) {
-        return await response.json();
-    }
+  let response = await fetch(url, {
+    method: "GET",
+  });
+  if (response.ok) {
+    return await response.json();
+  }
 }
 
 async function apiPost(url, payload) {
-    let response = await fetch(url, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(payload)
-    });
-    if (response.ok) {
-        // return console.log('ok')
-        return await response.json();
-    }
+  let response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+  if (response.ok) {
+    // return console.log('ok')
+    return await response.json();
+  }
 }
 
 async function apiDelete(url) {
-    await fetch(url, {
-        method: "DELETE",
-    });
+  await fetch(url, {
+    method: "DELETE",
+  });
 }
 
-async function apiPut(url) {
-}
+async function apiPut(url) {}
 
-async function apiPatch(url) {
-}
+async function apiPatch(url) {}
