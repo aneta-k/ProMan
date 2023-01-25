bcc536cfad9b0f8271202999c9583c01395ead83
TomaszCitko <citkotomasz@gmail.com>
2023-01-25 15:12:11 +0100

    small fix

--- static/js/data/dataHandler.js
+++ static/js/data/dataHandler.js
@@ -1,38 +1,40 @@
 export let dataHandler = {
-    getBoards: async function () {
-        return await apiGet("/api/boards");
-    },
-    getBoard: async function (boardId) {
-        // the board is retrieved and then the callback function is called with the board
-    },
-    getStatuses: async function () {
-        return await apiGet(`/api/statuses`);
-    },
-    getStatus: async function (statusId) {
-        // the status is retrieved and then the callback function is called with the status
-    },
-    getCardsByBoardId: async function (boardId) {
-        return await apiGet(`/api/boards/${boardId}/cards/`);
-    },
-    getCard: async function (cardId) {
-        // the card is retrieved and then the callback function is called with the card
-    },
-    createNewBoard: async function (boardTitle) {
-        return await apiPost(`/api/boards/create`, {'title':boardTitle});
-    },
-    createNewCard: async function (cardTitle, boardId, statusId) {
-        // creates new card, saves it and calls the callback function with its data
-    },
-    deleteCard: async function (cardId) {
-        await apiDelete(`/api/cards/${cardId}/delete`);
-    },
-    updateCardStatus: async function (cardId, statusId) {
-        await apiPatch(`/api/cards/${cardId}/status/update`, {'statusId':statusId});
-    },
-    changeBoardTitleApi: async function (boardID, title) {
-        let data = { boardId: boardID, title: title };
-        return await apiPut(`/api/board/${boardID}`, data);
-    },
+  getBoards: async function () {
+    return await apiGet("/api/boards");
+  },
+  getBoard: async function (boardId) {
+    // the board is retrieved and then the callback function is called with the board
+  },
+  getStatuses: async function () {
+    return await apiGet(`/api/statuses`);
+  },
+  getStatus: async function (statusId) {
+    // the status is retrieved and then the callback function is called with the status
+  },
+  getCardsByBoardId: async function (boardId) {
+    return await apiGet(`/api/boards/${boardId}/cards/`);
+  },
+  getCard: async function (cardId) {
+    // the card is retrieved and then the callback function is called with the card
+  },
+  createNewBoard: async function (boardTitle) {
+    return await apiPost(`/api/boards/create`, { title: boardTitle });
+  },
+  createNewCard: async function (cardTitle, boardId, statusId) {
+    // creates new card, saves it and calls the callback function with its data
+  },
+  deleteCard: async function (cardId) {
+    await apiDelete(`/api/cards/${cardId}/delete`);
+  },
+  updateCardStatus: async function (cardId, statusId) {
+    await apiPatch(`/api/cards/${cardId}/status/update`, {
+      statusId: statusId,
+    });
+  },
+  changeBoardTitleApi: async function (boardID, title) {
+    let data = { Id: boardID, title: title };
+    return await apiPut(`/api/board/${boardID}`, data);
+  },
 };
 
 async function apiGet(url) {
@@ -65,11 +67,11 @@ async function apiDelete(url) {
 async function apiPut(url) {}
 
 async function apiPatch(url, payload) {
-    await fetch(url, {
-        method: "PATCH",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-    });
+  await fetch(url, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
 }
