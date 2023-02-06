bc59f22517ac95d26a9c67eadc94c88244969988
Ostin-Oskarose <oskarcraft@gmail.com>
2023-02-06 11:37:23 +0100

    Updated sql file

--- data/proman_sample_data.sql
+++ data/proman_sample_data.sql
@@ -27,6 +27,7 @@ DROP TABLE IF EXISTS cards;
 
 CREATE TABLE statuses (
     id       SERIAL PRIMARY KEY     NOT NULL,
+    board_id INTEGER                NOT NULL,
     title    VARCHAR(200)           NOT NULL
 );
 
@@ -40,37 +41,47 @@ CREATE TABLE cards (
     board_id    INTEGER             NOT NULL,
     status_id   INTEGER             NOT NULL,
     title       VARCHAR (200)       NOT NULL,
-    card_order  INTEGER             NOT NULL
+    card_order  INTEGER             NOT NULL,
+    archived    BOOLEAN             NOT NULL
 );
 
 ---
 --- insert data
 ---
 
-INSERT INTO statuses(title) VALUES ('new');
-INSERT INTO statuses(title) VALUES ('in progress');
-INSERT INTO statuses(title) VALUES ('testing');
-INSERT INTO statuses(title) VALUES ('done');
 
 INSERT INTO boards(title) VALUES ('Board 1');
 INSERT INTO boards(title) VALUES ('Board 2');
 
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 1, 'new card 1', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 1, 'new card 2', 2);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 2, 'in progress card', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 3, 'planning', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 4, 'done card 1', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 4, 'done card 1', 2);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 1, 'new card 1', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 1, 'new card 2', 2);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 2, 'in progress card', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 3, 'planning', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 4, 'done card 1', 1);
-INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 4, 'done card 1', 2);
+INSERT INTO statuses(board_id, title) VALUES (1, 'new');
+INSERT INTO statuses(board_id, title) VALUES (1, 'in progress');
+INSERT INTO statuses(board_id, title) VALUES (1, 'testing');
+INSERT INTO statuses(board_id, title) VALUES (1, 'done');
+
+INSERT INTO statuses(board_id, title) VALUES (2, 'new');
+INSERT INTO statuses(board_id, title) VALUES (2, 'in progress');
+INSERT INTO statuses(board_id, title) VALUES (2, 'testing');
+INSERT INTO statuses(board_id, title) VALUES (2, 'done');
+
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 1, 'new card 1', 1, false);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 1, 'new card 2', 2, false);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 2, 'in progress card', 1, false);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 3, 'planning', 1, false);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 4, 'done card 1', 1, false);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 1, 4, 'done card 1', 2, false);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 1, 'new card 1', 1, false);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 1, 'new card 2', 2, false);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 2, 'in progress card', 1, false);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 3, 'planning', 1, false);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 4, 'done card 1', 1, false);
+INSERT INTO cards VALUES (nextval('cards_id_seq'), 2, 4, 'done card 1', 2, false);
 
 ---
 --- add constraints
 ---
+
+ALTER TABLE ONLY statuses
+    ADD CONSTRAINT fk_statuses_board_id FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE;
 
 ALTER TABLE ONLY cards
     ADD CONSTRAINT fk_cards_board_id FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE;
