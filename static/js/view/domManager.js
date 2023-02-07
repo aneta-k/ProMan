f1bced8d019f5930096bebfd91850a251af9afb6
aneta-k <kusnierz.aneta@gmail.com>
2023-02-07 18:32:43 +0100

    fixed some problems with changing names and display

--- static/js/view/domManager.js
+++ static/js/view/domManager.js
@@ -41,33 +41,33 @@ export let domManager = {
       console.error("could not find such html element: " + elementIdentifier);
     }
   },
-  changeDomBoardTitle(boardTitle) {
-    boardTitle.onclick = function () {
-      const inputText = document.createElement("input");
-      inputText.setAttribute("type", "text");
-      inputText.setAttribute("id", "tempTextBox");
-      inputText.setAttribute("value", boardTitle.innerText);
-      const saveButton = document.createElement("button");
-      saveButton.setAttribute("type", "button");
-      saveButton.setAttribute("id", "saveButton");
-      saveButton.setAttribute("value", "Save");
-      saveButton.setAttribute("class", "Button");
-      saveButton.innerText = "Save";
-      const textSpace = boardTitle.nextSibling;
-      boardTitle.replaceWith(inputText);
-      textSpace.replaceWith(saveButton);
-      inputText.focus();
-      saveButton.addEventListener("click", function (e) {
-        const inputText = document.getElementById("tempTextBox");
-        const saveButton = document.getElementById("saveButton");
-        const textSpace = " ";
-        const boardID = boardTitle.getAttribute("board-title-id");
-        boardTitle.innerText = inputText.value;
-        inputText.replaceWith(boardTitle);
-        saveButton.replaceWith(textSpace);
-        dataHandler.changeBoardTitleApi(boardID, inputText.value);
-      });
-    };
+  changeDomBoardTitle(clickEvent) {
+    const boardTitle = clickEvent.target;
+    const inputText = document.createElement("input");
+    inputText.setAttribute("type", "text");
+    inputText.setAttribute("id", "tempTextBox");
+    inputText.setAttribute("value", boardTitle.innerText);
+    inputText.classList.add('board-title');
+    const saveButton = document.createElement("button");
+    saveButton.setAttribute("type", "button");
+    saveButton.setAttribute("id", "saveButton");
+    saveButton.setAttribute("value", "Save");
+    saveButton.style.marginRight = '5px';
+    saveButton.innerText = "Save";
+    const textSpace = boardTitle.nextSibling;
+    boardTitle.replaceWith(inputText);
+    textSpace.replaceWith(saveButton);
+    inputText.focus();
+    inputText.setSelectionRange(inputText.value.length, inputText.value.length);
+
+    saveButton.addEventListener("click", async function (e) {
+      const textSpace = " ";
+      const boardId = boardTitle.dataset.boardId;
+      boardTitle.innerText = inputText.value;
+      inputText.replaceWith(boardTitle);
+      saveButton.replaceWith(textSpace);
+      await dataHandler.changeBoardTitleApi(boardId, inputText.value);
+    });
   },
   changeDomCardTitle(cardTitle) {
     cardTitle.onclick = function () {
@@ -77,6 +77,7 @@ export let domManager = {
       inputText.setAttribute("value", cardTitle.innerText);
       cardTitle.replaceWith(inputText);
       inputText.focus();
+      inputText.setSelectionRange(inputText.value.length, inputText.value.length);
 
       inputText.addEventListener("keydown", function (e) {
         if (e.key === "Enter") {
@@ -97,33 +98,30 @@ export let domManager = {
       });
     };
   },
-  changeDomColumnTitle(columnTitle) {
-    columnTitle.onclick = function () {
-      const inputText = document.createElement("input");
-      inputText.setAttribute("type", "text");
-      inputText.setAttribute("id", "tempTextBox");
-      inputText.setAttribute("value", columnTitle.innerText);
-      columnTitle.replaceWith(inputText);
-      inputText.focus();
+  changeDomColumnTitle(clickEvent) {
+    const columnTitle = clickEvent.target;
+    const inputText = document.createElement("input");
+    inputText.setAttribute("type", "text");
+    inputText.setAttribute("id", "tempTextBox");
+    inputText.setAttribute("value", columnTitle.innerText);
+    columnTitle.replaceWith(inputText);
+    inputText.focus();
+    inputText.setSelectionRange(inputText.value.length, inputText.value.length);
 
-      inputText.addEventListener("keydown", function (e) {
-        if (e.key === "Enter") {
-          const inputText = document.getElementById("tempTextBox");
-          const columnID = columnTitle.getAttribute("data-column-title-id");
-          console.log(columnID);
-          columnTitle.innerText = inputText.value;
-          inputText.replaceWith(columnTitle);
-          dataHandler.changeColumnTitleApi(columnID, inputText.value);
-        } else if (e.key === "Escape") {
+    inputText.addEventListener("keydown", async function (e) {
+      if (e.key === "Enter") {
+        const columnId = columnTitle.dataset.columnId;
+        columnTitle.innerText = inputText.value;
+        inputText.replaceWith(columnTitle);
+        await dataHandler.changeColumnTitleApi(columnId, inputText.value);
+      } else if (e.key === "Escape") {
+        inputText.replaceWith(columnTitle);
+      }
+      document.addEventListener("click", function (e) {
+        if (!inputText.contains(e.target)) {
           inputText.replaceWith(columnTitle);
         }
-        document.addEventListener("click", function (e) {
-          if (!inputText.contains(e.target)) {
-            inputText.replaceWith(columnTitle);
-          }
-        });
       });
-    };
+    });
   },
-
 };
