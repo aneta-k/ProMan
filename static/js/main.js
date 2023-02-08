2be2a3d78f4763d41fe5c12c7dee01dd1c31f56f
TomaszCitko <citkotomasz@gmail.com>
2023-02-08 18:02:09 +0100

    changes

--- static/js/main.js
+++ static/js/main.js
@@ -29,6 +29,43 @@ document.getElementById("refresh").addEventListener("click", function(){refresh_
 
 
 function refresh_boards() {
+  let openBoardsArray = clearTheBoards();
   domManager.deleteChildren(`.board-container`);
   init();
-}
+  setTimeout(() => {reOpenBoards(openBoardsArray)}, 2000);
+};
+
+function clearTheBoards() {
+  let openBoardsArray = [];
+  let boards = document.getElementsByClassName("board-toggle toggle-board-button");
+  for (let i = 0; i < boards.length; i++) {
+    if (boards[i].children[0].className == "fas fa-chevron-up"){
+      openBoardsArray.push(boards[i].attributes[1].value);
+    }
+  };
+return openBoardsArray
+};
+
+
+function reOpenBoards(openBoardsArray) {
+  console.log("openBoardsArray");
+  console.log(openBoardsArray);
+  let newBoards = document.getElementsByClassName("board-toggle toggle-board-button");
+  console.log("boards");
+  console.log(newBoards);
+  console.log(newBoards.length);
+  console.log(newBoards[1]);
+  for(let br in newBoards){
+    let trrr = newBoards[br];
+    console.log(trrr);
+    
+  }
+
+  for (let i = 0; i < newBoards.length; i++) {
+    console.log(newBoards);
+    
+    if (openBoardsArray.includes(newBoards[i].attributes[1].value)) {
+      newBoards[i].children[0].className = "fas fa-chevron-up";
+    }
+  };
+};
--- static/service-worker.js
+++ static/service-worker.js
@@ -35,7 +35,7 @@ self.addEventListener("install", e => {
 });
 
 self.addEventListener("fetch", e => {
-	console.log(`Intercepting ${e.request.url}`);
+	// console.log(`Intercepting ${e.request.url}`);
 	e.respondWith (
 		caches.match(e.request).then(response => {
 			return response || fetch(e.request);
