4e155c05cd639365ff2a56582620087e29ba7e28
Ostin-Oskarose <oskarcraft@gmail.com>
2023-02-08 13:49:55 +0100

    Replaced return ok

--- main.py
+++ main.py
@@ -140,41 +140,46 @@ def get_statuses(board_id):
 
 
 @app.route("/api/cards/<int:card_id>/delete", methods=['DELETE'])
+@json_response
 def delete_card(card_id):
     cards_handler.delete_card(card_id)
-    return 'ok'
+    return 200
 
 
 @app.route("/api/cards/<int:card_id>/status/update", methods=['PATCH'])
+@json_response
 def update_card_status(card_id):
     status = request.json['statusId']
     cards_handler.update_card_status(card_id, status)
-    return 'ok'
+    return 200
 
 
 @app.route('/api/cards/<int:card_id>/card_order/update', methods=['PATCH'])
+@json_response
 def update_cards_order(card_id):
     data = request.get_json()
     cards_handler.update_card_order(card_id, data['new_card_order'])
     cards_handler.update_cards_order(-1, data['old_card_order'], data['old_status'], data['board_id'], card_id)
     cards_handler.update_cards_order(1, data['new_card_order'], data['new_status'], data['board_id'], card_id)
-    return 'ok'
+    return 200
 
 
 @app.route('/api/cards/card_order_after_delete/update', methods=['PATCH'])
+@json_response
 def update_cards_order_after_delete():
     data = request.get_json()
     cards_handler.update_cards_order(-1, data['card_order'], data['status_id'], data['board_id'], data['card_id'])
-    return 'ok'
+    return 200
 
 
 @app.route('/api/cards/<int:card_id>/archived_status/update', methods=['PATCH'])
+@json_response
 def update_card_archived_status(card_id):
     new_archived_status = request.get_json()['new_status']
     new_card_order = request.get_json()['new_order']
     cards_handler.update_card_archived_status(card_id, new_archived_status)
     cards_handler.update_card_order(card_id, new_card_order)
-    return 'ok'
+    return 200
 
 
 @app.route("/api/board/<int:board_id>", methods=["PATCH"])
