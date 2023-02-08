4ef6417a9b9525a12936aa3ca0b58af1140a9f68
TomaszCitko <citkotomasz@gmail.com>
2023-02-08 18:38:09 +0100

    somehow working

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,10 @@
 import {boardsManager} from "./controller/boardsManager.js";
 import {userManager} from "./controller/userManager.js";
 import {modalManager} from "./controller/modalManager.js";
+import {columnManager} from "./controller/columnManager.js";
+import {cardsManager} from "./controller/cardsManager.js";
 import {domManager} from "./view/domManager.js";
+
 
 function init() {
     boardsManager.loadBoards();
@@ -12,27 +15,13 @@ function init() {
 
 init();
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register("/service-worker.js", { scope: '/' }).then(
-        registration => {
-          console.log(`Service Worker registered! Scope: ${registration.scope}`);
-        },
-        error => {
-          console.error(`Service Worker registration failed: ${error}`);
-        },
-      );
-    });
-  }
-
 document.getElementById("refresh").addEventListener("click", function(){refresh_boards()});
-
 
 function refresh_boards() {
   let openBoardsArray = clearTheBoards();
   domManager.deleteChildren(`.board-container`);
   init();
-  setTimeout(() => {reOpenBoards(openBoardsArray)}, 2000);
+  setTimeout(() => {reOpenBoards(openBoardsArray)}, 50);
 };
 
 function clearTheBoards() {
@@ -46,26 +35,41 @@ function clearTheBoards() {
 return openBoardsArray
 };
 
-
-function reOpenBoards(openBoardsArray) {
-  console.log("openBoardsArray");
-  console.log(openBoardsArray);
+async function reOpenBoards(openBoardsArray) {
   let newBoards = document.getElementsByClassName("board-toggle toggle-board-button");
-  console.log("boards");
   console.log(newBoards);
-  console.log(newBoards.length);
-  console.log(newBoards[1]);
-  for(let br in newBoards){
-    let trrr = newBoards[br];
-    console.log(trrr);
-    
-  }
-
   for (let i = 0; i < newBoards.length; i++) {
-    console.log(newBoards);
-    
     if (openBoardsArray.includes(newBoards[i].attributes[1].value)) {
       newBoards[i].children[0].className = "fas fa-chevron-up";
+      let boardId = newBoards[i].attributes[1].value;
+    if (domManager.hasChildren(`.board[data-board-id="${boardId}"] .board-columns`)) {
+        domManager.deleteChildren(`.board[data-board-id="${boardId}"] .board-columns`);
+        if (domManager.hasChildren(`.board-archived-container[data-board-id="${boardId}"]`)) {
+            domManager.deleteChildren(`.board-archived-container[data-board-id="${boardId}"]`);
+        }
+        if (document.querySelector(`form[data-board-id="${boardId}"]`)) {
+            domManager.deleteChildren(`#newFormField`);
+        }
+    } else {
+        await columnManager.loadColumns(boardId);
+        await cardsManager.loadCards(boardId);
+    }
     }
   };
 };
+
+
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register("/service-worker.js", { scope: '/' }).then(
+      registration => {
+        console.log(`Service Worker registered! Scope: ${registration.scope}`);
+      },
+      error => {
+        console.error(`Service Worker registration failed: ${error}`);
+      },
+    );
+  });
+}
